6122723b6708f1ebe6028a0b849847bbcd4298d8
G-Develop <mfaisal.rahman51.996@gmail.com>
2018-10-30 17:56:02 -0400

    googlemaps

--- routes/resources.js
+++ routes/resources.js
@@ -2,6 +2,16 @@ const express  = require("express");
 const router  = express.Router();
 const Resource = require("../models/resource");
 const middleware  = require("../middleware");
+const NodeGeocoder  = require('node-geocoder');
+
+let options = {
+  provider: 'google', 
+  httpAdapter:'https',
+  apiKey: process.env.GEOCODER_API_KEY,
+  formatter: null
+};
+
+const geocoder = NodeGeocoder(options);
 
 //will now render the INDEX  page that will show an index of the resources 
 router.get("/", function (req, res) {
@@ -17,28 +27,62 @@ router.get("/", function (req, res) {
   });
 });
 
-//CRUD CREATE via POST to /resources
-router.post("/",middleware.isLoggedIn, function (req, res) {
-  //get data from the form and add to the resources array object
-  console.log("user has hit the post route");
+
+//CREATE - add new resource to DB
+router.post("/", middleware.isLoggedIn, function(req, res){
+  // get data from form and add to resources array
   let name = req.body.name;
   let image = req.body.image;
   let caption = req.body.caption;
   let author = {
-    id: req.user._id,
-    username: req.user.username
+      id: req.user._id,
+      username: req.user.username
   }
-  let newResource = {name: name, image: image, caption: caption, author: author}
-  //Create a new resource and save to DB
-  Resource.create(newResource, function(err, freshResource) {
-    if (err){
-      console.log(err);
-    } else {
-      console.log(freshResource);
-      res.redirect("/resources");
+  geocoder.geocode(req.body.location, function (err, data) {
+    if (err || !data.length) {
+      req.flash('error', 'Invalid address');
+      return res.redirect('back');
     }
+    let lat = data[0].latitude;
+    let lng = data[0].longitude;
+    let location = data[0].formattedAddress;
+    let newResource = {name: name, image: image, caption: caption, author:author, location: location, lat: lat, lng: lng};
+    // Create a new resource and save to DB
+    Resource.create(newResource, function(err, freshResource){
+        if(err){
+            console.log(err);
+        } else {
+            console.log(freshResource);
+            res.redirect("/resources");
+        }
+    });
   });
 });
+
+
+
+////CRUD CREATE via POST to /resources
+//router.post("/",middleware.isLoggedIn, function (req, res) {
+  ////get data from the form and add to the resources array object
+  //console.log("user has hit the post route");
+  //let name = req.body.name;
+  //let image = req.body.image;
+  //let caption = req.body.caption;
+  //let author = {
+    //id: req.user._id,
+    //username: req.user.username
+  //}
+  //let newResource = {name: name, image: image, caption: caption, author: author}
+  ////Create a new resource and save to DB
+  //Resource.create(newResource, function(err, freshResource) {
+    //if (err){
+      //console.log(err);
+    //} else {
+      //console.log(freshResource);
+      //res.redirect("/resources");
+    //}
+  //});
+//});
 
 
 //NEW Form that will help send the POST  to /resources 
@@ -73,16 +117,45 @@ router.get("/:id/edit", middleware.checkResourceOwnership, function (req, res) {
 });
 
 
-//Update  for edit RESOURCE form
-router.put("/:id", middleware.checkResourceOwnership, function(req,res) {
-  Resource.findByIdAndUpdate(req.params.id, req.body.resource, function (err, updatedResource) {
-   if(err){
-     res.redirect("/resources");
-   } else {
-     res.redirect("/resources/" + req.params.id);
-   }
+
+
+// UPDATE RESOURCE ROUTE
+router.put("/:id", middleware.checkResourceOwnership, function(req, res){
+  geocoder.geocode(req.body.location, function (err, data) {
+    if (err || !data.length) {
+      req.flash('error', 'Invalid address');
+      return res.redirect('back');
+    }
+    req.body.resource.lat = data[0].latitude;
+    req.body.resource.lng = data[0].longitude;
+    req.body.resource.location = data[0].formattedAddress;
+
+    Resource.findByIdAndUpdate(req.params.id, req.body.resource, function(err, resource){
+        if(err){
+            req.flash("error", err.message);
+            res.redirect("back");
+        } else {
+            req.flash("success","Updated!");
+            res.redirect("/resources/" + resource._id);
+        }
+    });
   });
 });
+
+
+
+
+
+////Update  for edit RESOURCE form
+//router.put("/:id", middleware.checkResourceOwnership, function(req,res) {
+  //Resource.findByIdAndUpdate(req.params.id, req.body.resource, function (err, updatedResource) {
+   //if(err){
+     //res.redirect("/resources");
+   //} else {
+     //res.redirect("/resources/" + req.params.id);
+   //}
+  //});
+//});
 
 //DESTORY RESOURCE ROUTE
 router.delete("/:id", middleware.checkResourceOwnership, function (req, res) {
