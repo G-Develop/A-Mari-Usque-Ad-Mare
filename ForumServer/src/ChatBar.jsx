ba9b13d531727c9e2245491547f69225479b4b8f
G-Develop <mfaisal.rahman51.996@gmail.com>
2018-10-31 13:47:53 -0400

    used e.target.value = empty string to clear chat

--- ForumServer/src/ChatBar.jsx
+++ ForumServer/src/ChatBar.jsx
@@ -2,15 +2,13 @@ import React, {Component} from 'react';
 
 class ChatBar extends Component {
 
-
-
-
   //handles when Enter is pressed in the message input element
   handle_eMessageEnter = e => {
     if (e.keyCode == 13) { // keycode 13 is the enter key
       console.log("enter detected")
       e.preventDefault();
       this.props.appendMessage(e.target.value);
+      e.target.value = "";
     }
   }
   // handles when Enter is pressed in the name input element to allow for a name change
@@ -32,7 +30,6 @@ class ChatBar extends Component {
         className="chatbar-username"
         placeholder="Your Name (Optional)"
       />
-
       <input name="message"
         onKeyDown={this.handle_eMessageEnter}
         className="chatbar-message"
